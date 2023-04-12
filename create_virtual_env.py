b5fee85f84d816b1f4a65ca44957237a8524cfea
Mirko Schuerlein <mirkoschuerlein@googlemail.com>
2023-04-12 13:32:29 +0200

    only print when created, simplify code

--- create_virtual_env.py
+++ create_virtual_env.py
@@ -9,16 +9,7 @@ Created on Fri May 21 14:15:00 2021
 import os
 
 def create_virtual_env(name, directory):
-    
-    env_list = os.popen('conda env list').read()
-    exist = name in env_list
-    
-    if not exist:       
-        os.system('conda env create -f' + directory + '/' + 'environment.yml')
-        
-        return 1
-    
-    else:
-        print('The environment is already exists')
-        return 0
 
+    if not name in os.popen('conda env list').read():       
+        os.system('conda env create -f' + os.path.join(directory, 'environment.yml'))
+        print(f"conda environment '{name}' was created")
